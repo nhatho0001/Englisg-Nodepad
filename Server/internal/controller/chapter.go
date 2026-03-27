4aced84a3fe6a7ace644d2b6389eafd0d04b4b77
Nhat Ho <nhatho0001@gmail.com>
2026-03-27 16:11:33 +0700

    create api for create vocabulary of chapter

--- Server/internal/controller/chapter.go
+++ Server/internal/controller/chapter.go
@@ -2,15 +2,34 @@ package controller
 
 import (
 	"app-notepad/internal/services"
+	"app-notepad/internal/store"
 	"log/slog"
 	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 type ChapterHander struct {
 	Chapter *services.ChapterService
+}
+
+type ChapterRequest struct {
+	Chapter         store.CreateChapterParams
+	List_Vocabulary []store.CreateVocabularyParams
+}
+
+type ChapterResponse struct {
+	Chapter         store.Chapter
+	List_Vocabulary []store.Vocabulary
+}
+
+func (c *ChapterRequest) ValidateDataInput() bool {
+	if !c.Chapter.Title.Valid || len(c.List_Vocabulary) == 0 {
+		return false
+	}
+	return true
 }
 
 func NewChapterHander(s *services.ChapterService) *ChapterHander {
@@ -50,4 +69,67 @@ func (ch *ChapterHander) GetListChapter(c *gin.Context) {
 	c.AbortWithStatusJSON(http.StatusOK, gin.H{
 		"data": chapter_list,
 	})
+}
+
+func (ch *ChapterHander) CreateChapter(c *gin.Context) {
+	var data ChapterRequest
+	if err := c.ShouldBindBodyWithJSON(&data); err != nil {
+		slog.Error(err.Error())
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if !data.ValidateDataInput() {
+		slog.Error("Field request is not have")
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Field request is not have",
+		})
+		return
+	}
+
+	uid, exist := c.Get("uid")
+	if !exist {
+		slog.Error("Error account of you is not verify")
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Error account of you is not verify",
+		})
+		return
+	}
+	uid_int, err := strconv.Atoi(uid.(string))
+	if err != nil {
+		slog.Error(err.Error())
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	data.Chapter.UserID = int32(uid_int)
+
+	new_chapter, err := ch.Chapter.CreateChapter(c, &data.Chapter)
+	if err != nil {
+		slog.Error(err.Error())
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	new_list_vocabulary := ch.Chapter.CreateVocabularyOfService(c, data.List_Vocabulary, pgtype.Int4{
+		Int32: new_chapter.ID,
+		Valid: true,
+	})
+	if len(new_list_vocabulary) == 0 {
+		slog.Error("Error not vocabulary is created")
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"error": "Error not vocabulary is created",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, ChapterResponse{
+		Chapter:         *new_chapter,
+		List_Vocabulary: new_list_vocabulary,
+	})
+
 }
--- Server/internal/services/chapter_service.go
+++ Server/internal/services/chapter_service.go
@@ -4,6 +4,8 @@ import (
 	"app-notepad/configs"
 	"app-notepad/internal/store"
 	"context"
+
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 type ChapterService struct {
@@ -15,10 +17,33 @@ func NewChapterService(q *store.Queries, cfg *configs.Configs) *ChapterService {
 	return &ChapterService{query: q, cfg: cfg}
 }
 
-func (chepter *ChapterService) GetListChapter(ctx context.Context, uid int32) ([]store.Chapter, error) {
-	list_chapter, err := chepter.query.GetChaptersByUser(ctx, uid)
+func (chapter *ChapterService) GetListChapter(ctx context.Context, uid int32) ([]store.Chapter, error) {
+	list_chapter, err := chapter.query.GetChaptersByUser(ctx, uid)
 	if err != nil {
 		return nil, err
 	}
 	return list_chapter, nil
+}
+
+func (chapter *ChapterService) CreateChapter(ctx context.Context, arg *store.CreateChapterParams) (*store.Chapter, error) {
+	if arg.Status.String == "" {
+		arg.Status.String = "new"
+	}
+	new_chapter, err := chapter.query.CreateChapter(ctx, *arg)
+	if err != nil {
+		return nil, err
+	}
+	return &new_chapter, nil
+}
+
+func (chapter *ChapterService) CreateVocabularyOfService(ctx context.Context, arg []store.CreateVocabularyParams, chapeter_id pgtype.Int4) []store.Vocabulary {
+	var result []store.Vocabulary
+	for _, v := range arg {
+		v.ChapterID = chapeter_id
+		new_vocabulary, err := chapter.query.CreateVocabulary(ctx, v)
+		if err == nil {
+			result = append(result, new_vocabulary)
+		}
+	}
+	return result
 }
